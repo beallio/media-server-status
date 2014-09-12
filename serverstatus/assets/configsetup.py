9fa7c6951a531e759a5ba55f4817c481f4c1f67e
beallio <david@beallio.com>
2014-09-12 01:55:43 -0700

    * Clean-up logging * Add MIT License

--- serverstatus/assets/configsetup.py
+++ serverstatus/assets/configsetup.py
@@ -4,25 +4,34 @@ import logging
 import logging.handlers as handlers
 
 # setup application logger
-log_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../tmp')
+# use dir name thrice to return to base module path
+log_directory = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))), 'tmp')
+print log_directory
 log_location = os.path.join(log_directory, 'status.log')
 if not os.path.isdir(log_directory):
-    os.mkdir(log_directory)
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
-                    filemode='a')
+    try:
+        os.mkdir(log_directory)
+    except IOError:
+        pass
+if os.path.isdir(log_directory):
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filemode='a')
 
-file_handler = handlers.RotatingFileHandler(filename=log_location, maxBytes=3145728)
-logging.getLogger('').addHandler(file_handler)
-logger = logging.getLogger(__name__)
-logger.debug('Logger initialized')
-
+    file_handler = handlers.RotatingFileHandler(filename=log_location, maxBytes=3145728)
+    logging.getLogger('').addHandler(file_handler)
+    logger = logging.getLogger(__name__)
+    logger.debug('Logger initialized')
+else:
+    logger = None
 
 # import config file
 try:
     config = imp.load_source('test_data', '/var/test_data.py')
-    logger.info('Config file loaded')
+    if logger:
+        logger.info('Config file loaded')
 except IOError:
-    logger.error('Config file not found')
+    if logger:
+        logger.error('Config file not found')
     config = None
