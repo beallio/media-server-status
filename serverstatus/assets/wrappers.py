a66f3d2b313301c20e0d6960592b11a4c965112a
David Beall <davidbeall@gmail.com>
2014-10-02 19:22:21 +0000

    updated docstrings

--- serverstatus/assets/wrappers.py
+++ serverstatus/assets/wrappers.py
@@ -1,9 +1,21 @@
+"""
+function wrappers module
+"""
+
 import logging
 from inspect import stack, getmodule
 
 
 def logger(log_type):
+    """
+    decorator to log output of functions
+    :param log_type: logger level as string (debug, warn, info, etc)
+    :type log_type: str
+    """
     def log_decorator(func):
+        """
+        wrapped function
+        """
         def wrapped(*args, **kwargs):
             # preserve calling module name for LOGGER
             frm = stack()[1]
@@ -12,8 +24,8 @@ def logger(log_type):
             result = func(*args, **kwargs)
             try:
                 getattr(wrapped_logger, log_type)(result)
-            except AttributeError as e:
-                wrapped_logger.error(e)
+            except AttributeError as err:
+                wrapped_logger.error(err)
             return result
 
         return wrapped
@@ -22,8 +34,10 @@ def logger(log_type):
 
 
 def log_args(function):
+    """
+    Logs arguments passed to function
+    """
     def wrapper(*args, **kwargs):
         print 'Arguments:', args, kwargs
         return function(*args, **kwargs)
-
     return wrapper
